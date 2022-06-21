4f98198e7d0755534356faefe830278ed71b8cea
grilledcheeseplease <lacynunnally402@gmail.com>
2022-06-21 15:58:47 -0400

    working on tests employee passes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,7 +5,7 @@ describe('Employee', () => {
 
   it('Can instantiate Employee instance', () => {
     const e = new Employee();
-    expect(typeof(e)).toBe('object');
+    expect(typeof (e)).toBe('object');
   });
 
   it('Should get the employee\'s name', () => {
@@ -24,6 +24,38 @@ describe('Employee', () => {
     const testValue = 'ron@swanson.com';
     const e = new Employee('Foo', 1, testValue);
     expect(e.email).toBe(testValue);
+  });
+
+  describe('getName', () => {
+    it('Can get name via getName()', () => {
+      const testValue = 'Ron';
+      const e = new Employee(testValue);
+      expect(e.getName()).toBe(testValue);
+    });
+  });
+
+  describe('getId', () => {
+    it('Can get id via getId()', () => {
+      const testValue = 800;
+      const e = new Employee('Foo', testValue);
+      expect(e.getId()).toBe(testValue);
+    });
+  });
+
+  describe('getEmail', () => {
+    it('Can get email via getEmail()', () => {
+      const testValue = 'ron@swanson.com';
+      const e = new Employee('Foo', 1, testValue);
+      expect(e.getEmail()).toBe(testValue);
+    });
+  });
+
+  describe('getRole', () => {
+    it('getRole() should return \'Employee\'', () => {
+      const testValue = 'Employee';
+      const e = new Employee('Ron', 1, 'ron@swanson.com');
+      expect(e.getRole()).toBe(testValue);
+    });
   });
 
 });
